f9927a2fb3f9c34c729f827b976886991ae4c6b3
vmarialuz <vmarialuzm@gmail.com>
2023-11-28 16:01:16 -0500

    feat: detail

--- todo/detail.js
+++ todo/detail.js
@@ -2,24 +2,31 @@ const titleElement = document.querySelector("#title");
 const desc = document.querySelector("#desc");
 const created = document.querySelector("#created");
 const statusElement = document.querySelector("#status");
-const btnDelete = document.querySelector("#btn-delete")
+/* const btnDelete = document.querySelector("#btn-delete"); */
+const button = document.querySelector("#button");
 
 const getTask = async () => {
     // Paso1: Obtener los datos de la url
-    const response = await fetch(`http://127.0.0.1:8000/api/v3/todo/${getId()}`);
+    const response = await fetch(`http://127.0.0.1:8000/v3/todo3/${getId()}`);
     const {title, body, created_at, status} = await response.json();  //desestructuracion
     titleElement.textContent = title;
     desc.textContent = body;
     dayjs.extend(dayjs_plugin_relativeTime);
     created.textContent = `Hace ${dayjs(created_at).fromNow()}`;
     statusElement.textContent = status;
+
+    button.innerHTML = `
+    <a href="./index.html" class="btn btn-primary">Regresar</a>
+    <a href="./edit.html?id=${getId()}" class="btn btn-primary">Editar</a>
+    <button onclick="deleteTodo()" class="btn btn-danger">Eliminar</button>
+    `
 };
 
 getTask();
 
 /* ------------------delete------------------ */
 
-btnDelete.onclick = async function() {
+const deleteTodo = async function() {
     const {value} = await Swal.fire({
         title: 'Está seguro de eliminar este registro?',
         showDenyButton: true,
@@ -31,7 +38,7 @@ btnDelete.onclick = async function() {
     if (value) {
         try {
             await fetch(
-                `http://127.0.0.1:8000/api/v1/todo/${getId()}`,
+                `http://127.0.0.1:8000/v3/todo3/${getId()}`,
                 {
                     method: "DELETE",
                     headers: {
